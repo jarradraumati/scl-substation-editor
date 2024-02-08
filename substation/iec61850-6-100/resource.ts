f3ee5ab2bc5524fb8370c7c789cf345a7907d1b2
Jarrad Raumati <jarrad.raumati@gmail.com>
2024-02-08 19:58:01 +1300

    Updates

--- substation/iec61850-6-100/resource.ts
+++ substation/iec61850-6-100/resource.ts
@@ -5,24 +5,56 @@ import { customElement, state } from 'lit/decorators.js';
 
 import '@openscd/oscd-action-pane';
 
+import { createElement } from '@openenergytools/scl-lib/dist/foundation/utils.js';
 import { getChildElementsByTagName } from '../../foundation.js';
+import { getLNodeIcon } from '../l-node-editor.js';
 import BaseSubstationElementEditor from '../base-substation-element-editor.js';
+
+export function getLNodeFromSource(resource: Element): Element {
+  const source = resource
+    .getAttribute('source')
+    ?.split('/')
+    .slice(0, -1)
+    .map(a => `[name="${a}"]`)
+    .join(' > ');
+  const sourceLNode = resource
+    .getAttribute('source')
+    ?.split('/')
+    .at(-1)
+    ?.slice(-5, -1);
+
+  const lnode = resource.ownerDocument.querySelector(
+    `${source} > LNode[lnClass*="${sourceLNode}"]`
+  );
+  if (!lnode)
+    return createElement(resource.ownerDocument, 'LNode', { lnClass: 'Z' });
+  return lnode;
+}
 
 /** Pane rendering `Resource` element with its children */
 @customElement('resource-editor')
 export class ResourceEditor extends BaseSubstationElementEditor {
   @state()
   private get header(): string {
-    const resInst = this.element.getAttribute('resInst');
+    const source = this.element.getAttribute('source')?.split('/').at(-1);
 
-    return `${resInst ? ` ${resInst}` : ''}`;
+    return `${source ? ` ${source}` : ''}`;
+  }
+
+  @state()
+  private get missingSourceReference(): boolean {
+    return this.element.getAttribute('source') === '';
   }
 
   render(): TemplateResult {
     if (this.element.namespaceURI === 'http://www.iec.ch/61850/2019/SCL/6-100')
       this.is6100 = true;
-    return html`<oscd-action-icon label="${this.header}" secondary
-      ><mwc-icon slot="icon">commit</mwc-icon
+    return html`<oscd-action-icon
+      label="${this.header}"
+      ?secondary=${this.missingSourceReference}
+      ?highlighted=${this.missingSourceReference}
+      ><mwc-icon slot="icon"
+        >${getLNodeIcon(getLNodeFromSource(this.element))}</mwc-icon
       ><mwc-fab
         class="action edit"
         slot="action"
